cc814cf97103413b5d865aae6ea2a9f29f44242d
TrevorBeed <143837899+TrevorBeed@users.noreply.github.com>
2024-01-23 15:28:02 -0600

    Fav and Name
    Fav and Name

---
+++ Name.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+
+
+
+func _on_btn_show_pressed():
+	$Label.text = "Trevor Beed"
+
+
+func _on_btn_clear_pressed():
+	$Label.text = ""
+
+
+func _on_btn_exit_pressed():
+	get_tree().quit()

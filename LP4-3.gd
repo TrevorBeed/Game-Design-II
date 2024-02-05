e92f9fba43151251c370d8e354a2012da6da539b
TrevorBeed <143837899+TrevorBeed@users.noreply.github.com>
2024-02-05 15:14:23 -0600

    eggs

--- LP4-3.gd
+++ LP4-3.gd
@@ -5,11 +5,17 @@ func _on_btn_calc_pressed():
 	var dozen = egg / 12
 	var LO = egg - (dozen * 12)
 	var price = 0
-	if dozen > 0 and dozen <= 3:
+	if dozen >= 0 and dozen < 3:
 		price = dozen * 0.50
-	elif dozen > 3 and dozen < 6:
+	elif dozen >= 3 and dozen < 6:
 		price = dozen * 0.45
-	
+	elif dozen >= 6 and dozen < 11:
+		price = dozen * 0.40
+	elif dozen >= 11:
+		price = dozen * 0.35
+	var ep = LO * (price/12)
+	var fp = ep + price
+	$lblOut.text = "$" + str(fp)
 
 func _on_btn_clear_pressed():
 	$lblOut.text = ""

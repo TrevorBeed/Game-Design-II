a42eba49386f531cdd0c3e4e4a0ca1d964b1fe80
TrevorBeed <143837899+TrevorBeed@users.noreply.github.com>
2024-04-03 15:26:04 -0500

    Scope

--- fps_player_template.gd
+++ fps_player_template.gd
@@ -53,6 +53,33 @@ var reload_sound = preload("res://assets/sounds/recharge.mp3")
 var hit_sound = preload("res://assets/sounds/hitHurt.wav")
 var dink_sound = preload("res://assets/sounds/hitHead.wav")
 
+var unaim_pos = Vector3(0.219, -0.27, -0.421)
+var aim_pos = Vector3(0, -0.14, -0.511)
+var unaim_quat = euler_degrees_to_quat(Vector3(28.1, 31.7, 0))
+var aim_quat = euler_degrees_to_quat(Vector3(11.6, 0, 0))
+var target_pos = unaim_pos
+var target_quat = unaim_quat
+
+func degrees_to_radians(degrees: Vector3) -> Vector3:
+	return Vector3(
+		deg_to_rad(degrees.x),
+		deg_to_rad(degrees.y),
+		deg_to_rad(degrees.z)
+	)
+
+
+func radians_to_degrees(radians: Vector3) -> Vector3:
+	return Vector3(
+		rad_to_deg(radians.x),
+		rad_to_deg(radians.y),
+		rad_to_deg(radians.z)
+	)
+
+
+func euler_degrees_to_quat(euler_degrees: Vector3) -> Quaternion:
+	return Quaternion.from_euler(degrees_to_radians(euler_degrees))
+
+
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -113,6 +140,19 @@ func _physics_process(delta):
 	$HUD/Label2/lblAmmo.text = str(int(AMMO)) + "/" + str(TOTAL_AMMO)
 	if damage_lock == 0:
 		$HUD/overlay.material = null
+	
+	if Input.is_action_pressed("aim_sight"):
+		target_pos = aim_pos
+		target_quat = aim_quat
+		$Head/Camera3D.fov = lerp(45, 75, delta * 10.0)
+	elif Input.is_action_just_released("aim_sight"):
+		target_pos = unaim_pos
+		target_quat = unaim_quat
+		$Head/Camera3D.fov = lerp(75, 45, delta * 10.0)
+	blaster.position = blaster.position.lerp(target_pos, delta * 10.0)
+	var cur_quat = Quaternion.from_euler(degrees_to_radians(blaster.rotation_degrees))
+	blaster.rotation_degrees = radians_to_degrees(cur_quat.slerp(target_quat, delta * 10.0).get_euler())
+	
 	
 	if Input.is_action_pressed("crouch"):
 		$CollisionShape3D.shape.height = CROCUH_HEIGHT + 0.05

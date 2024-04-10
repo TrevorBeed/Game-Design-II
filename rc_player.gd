c195dc7561bb9b9019dcca3b1541f23e618995ef
TrevorBeed <143837899+TrevorBeed@users.noreply.github.com>
2024-04-10 15:28:10 -0500

    Racing camera

--- rc_player.gd
+++ rc_player.gd
@@ -17,6 +17,17 @@ func _physics_process(delta):
 	$backleft.engine_force = calc_engine_force(accel, abs($backleft.get_rpm()))
 	$backright.engine_force = calc_engine_force(accel, abs($backright.get_rpm()))
 	
-	#TODO: mph label
-	#TODO: right vehical
-	#TODO: Chase camera
+	var fwd_mps = abs((self.linear_velocity * self.transform.basis).z)
+	$Label.text = "%d mph" % (fwd_mps * 2.23694)
+	
+	$centerMass.global_position = $centerMass.global_position.lerp(self.global_position, delta * 20.0)
+	$centerMass.transform = $centerMass.transform.interpolate_with(self.transform, delta * 5.0)
+	$centerMass/Camera3D.look_at(self.global_position.lerp(self.global_position + self.linear_velocity, delta * 5.0))
+	check_and_right_vehicle()
+
+func check_and_right_vehicle():
+	if self.global_transform.basis.y.dot(Vector3.UP) < 0:
+		var current_rotation = self.rotation_degrees
+		current_rotation.x = 0 #reset pitch
+		current_rotation.z = 0 #reset roll
+		self.rotation_degrees = current_rotation

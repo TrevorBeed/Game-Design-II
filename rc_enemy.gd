dbc8528798ca5a0d67eb323a8f29a1bf730adc17
TrevorBeed <143837899+TrevorBeed@users.noreply.github.com>
2024-04-26 15:28:04 -0500

    Sound

--- rc_enemy.gd
+++ rc_enemy.gd
@@ -4,17 +4,17 @@ const MAX_STEER = 0.4
 const MAX_RPM = 150
 const MAX_TORQUE = 500
 const HORSE_POWER = 75
-const REVERSE_POWER = -HORSE_POWER * 2 #-50
+const REVERSE_POWER = -HORSE_POWER * 2  # -50
 const STUCK_TIME_THRESHOLD = 0.5
 
 var stuck_timer = 0.0
 var is_stuck = false
 
-@onready var rayF = $rayForward # y=0
-@onready var rayFL = $rayForwLeft # y=30 
-@onready var rayFR = $rayForwRight # y=-30
-@onready var rayL = $rayLeft # y=75
-@onready var rayR = $rayRight # y=-75
+@onready var rayF  = $rayForward    # y=0
+@onready var rayFL = $rayForwLeft   # y=30
+@onready var rayFR = $rayForwRight  # y=-30
+@onready var rayL  = $rayLeft       # y=75
+@onready var rayR  = $rayRight      # y=-75
 
 func _physics_process(delta):
 	var target_steering = 0.0
@@ -22,23 +22,24 @@ func _physics_process(delta):
 	
 	var acceleration = HORSE_POWER
 	if rayF.is_colliding():
-		var dist_to_obstacle = rayF.get_collision_point().distance_to(self.global_transform.origin)
+		var dist_to_obstacle = rayF.get_collision_point(). \
+									distance_to(self.global_transform.origin)
 		acceleration *= max(0.1, dist_to_obstacle / 10.0)
-		
+	
 	if rayFL.is_colliding() or rayL.is_colliding():
-		target_steering -= MAX_STEER
+		target_steering -= MAX_STEER  # Steer away from obstacle
 	if rayFR.is_colliding() or rayR.is_colliding():
 		target_steering += MAX_STEER
-		
+	
 	if is_stuck:
 		acceleration = REVERSE_POWER
-		steering = -sin(steering) * MAX_STEER
+		steering = -sign(steering) * MAX_STEER
 	
-	$backleft.engine_force = calc_engine_force(acceleration, abs($backleft.get_rpm()))
-	$backright.engine_force = calc_engine_force(acceleration, abs($backright.get_rpm()))
+	$backLeft.engine_force = calc_engine_force(acceleration, abs($backLeft.get_rpm()))
+	$backRight.engine_force = calc_engine_force(acceleration, abs($backRight.get_rpm()))
 	check_and_right_vehicle()
-		
 	
+
 func check_stuck_condition(delta):
 	if linear_velocity.length() < 1.0:
 		stuck_timer += delta
@@ -46,8 +47,6 @@ func check_stuck_condition(delta):
 		stuck_timer = 0
 	is_stuck = stuck_timer > STUCK_TIME_THRESHOLD
 
-func calc_engine_force(accel,rpm):
-	return accel * MAX_TORQUE * (1 - rpm / MAX_RPM)
 
 func update_raycasts():
 	rayF.force_raycast_update()
@@ -57,9 +56,13 @@ func update_raycasts():
 	rayR.force_raycast_update()
 
 
+func calc_engine_force(accel, rpm):
+	return accel * MAX_TORQUE * (1 - rpm / MAX_RPM)
+
+
 func check_and_right_vehicle():
 	if self.global_transform.basis.y.dot(Vector3.UP) < 0:
 		var current_rotation = self.rotation_degrees
-		current_rotation.x = 0 #reset pitch
-		current_rotation.z = 0 #reset roll
+		current_rotation.x = 0  # Reset pitch
+		current_rotation.z = 0  # Reset roll
 		self.rotation_degrees = current_rotation
--- rc_player.gd
+++ rc_player.gd
@@ -5,6 +5,11 @@ const MAX_RPM = 300
 const MAX_TORQUE = 200
 const HORSE_POWER = 100
 
+@onready var audio_player = $AudioStreamPlayer3D
+var moving = preload("res://assets/sounds/transport_car_drive_fast_race_engine_perspective.mp3")
+var idle = preload("res://assets/sounds/zapsplat_vehicles_car_diesel_engine_idle_close_under_engine_nissan_patrol_1998_67744.mp3")
+
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 
@@ -16,6 +21,12 @@ func _physics_process(delta):
 	var accel = Input.get_axis("ui_down", "ui_up") * HORSE_POWER
 	$backleft.engine_force = calc_engine_force(accel, abs($backleft.get_rpm()))
 	$backright.engine_force = calc_engine_force(accel, abs($backright.get_rpm()))
+	if accel > 0:
+		if audio_player.playing:
+			await audio_player.finished
+		audio_player.stream = moving if accel <= 0 else idle
+		audio_player.play()
+		
 	
 	var fwd_mps = abs((self.linear_velocity * self.transform.basis).z)
 	$Label.text = "%d mph" % (fwd_mps * 2.23694)
